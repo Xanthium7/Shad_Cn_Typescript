4a599dbff497fa741965d5cc74d4a44f19b01676
Akshhay <=>
2023-09-28 15:09:38 +0530

    added todo list without the rendering feature

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@
 // import { Button } from "@/components/ui/ui/button";
 import { ModeToggle } from "@/components/ui/ui/toggle-mode";
 import { BellIcon, CheckIcon } from "@radix-ui/react-icons";
-
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
 import {
   Card,
   CardContent,
@@ -15,73 +15,54 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Switch } from "@/components/ui/switch";
-
-const notifications = [
-  {
-    title: "Your call has been confirmed.",
-    description: "1 hour ago",
-  },
-  {
-    title: "You have a new message!",
-    description: "1 hour ago",
-  },
-  {
-    title: "Your subscription is expiring soon!",
-    description: "2 hours ago",
-  },
-];
+import { useState } from "react";
+import { log } from "console";
+// import { Switch } from "@/components/ui/switch";
 
 export default function Home() {
+  const todoList: string[] = [];
+  const [textArea, settextArea] = useState("");
   return (
     <main className="flex gap-10 min-h-screen flex-col items-center justify-center p-24">
       <div className="flex justify-between w-full">
         <h1 className="text-3xl font-semibold ">SHAD CN TESTING</h1>
         <ModeToggle />
       </div>
-      <Button>Button</Button>
+      <Separator />
+      <h1>TODO APP</h1>
 
       <Card className={"w-[680px]"}>
         <CardHeader>
-          <CardTitle>Notifications</CardTitle>
-          <CardDescription>You have 3 unread messages.</CardDescription>
+          <CardTitle>Todo App</CardTitle>
+          <CardDescription>Add Yout Todos below</CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
-          <div className=" flex items-center space-x-4 rounded-md border p-4">
-            <BellIcon />
-            <div className="flex-1 space-y-1">
-              <p className="text-sm font-medium leading-none">
-                Push Notifications
-              </p>
-              <p className="text-sm text-muted-foreground">
-                Send notifications to device.
-              </p>
-            </div>
-            <Switch />
-          </div>
-          <div>
-            {notifications.map((notification, index) => (
-              <div
-                key={index}
-                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-              >
-                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                <div className="space-y-1">
-                  <p className="text-sm font-medium leading-none">
-                    {notification.title}
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    {notification.description}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
+          <Textarea
+            value={textArea}
+            onChange={(e) => {
+              settextArea(e.target.value);
+            }}
+            placeholder="Type your message here."
+          />
+          <Button
+            onClick={() => {
+              todoList.push(textArea);
+              settextArea("");
+              console.log(todoList);
+            }}
+          >
+            Add Todo
+          </Button>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">
-            <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
-          </Button>
+          <Separator />
+          {/* insert todo here */}
+          {todoList.map((item, index) => (
+            <div>
+              <h1>hello</h1>
+              <Separator />
+            </div>
+          ))}
         </CardFooter>
       </Card>
     </main>
