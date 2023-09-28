1c6fe2dfadb9cd095bf473570a249cab41732905
Akshhay <=>
2023-09-28 11:30:58 +0530

    added a form with custamisable theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,89 @@
+"use client";
+
+// import { Button } from "@/components/ui/ui/button";
+import { ModeToggle } from "@/components/ui/ui/toggle-mode";
+import { BellIcon, CheckIcon } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
+
+const notifications = [
+  {
+    title: "Your call has been confirmed.",
+    description: "1 hour ago",
+  },
+  {
+    title: "You have a new message!",
+    description: "1 hour ago",
+  },
+  {
+    title: "Your subscription is expiring soon!",
+    description: "2 hours ago",
+  },
+];
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      SHAD CN TESTING
+    <main className="flex gap-10 min-h-screen flex-col items-center justify-center p-24">
+      <div className="flex justify-between w-full">
+        <h1 className="text-3xl font-semibold ">SHAD CN TESTING</h1>
+        <ModeToggle />
+      </div>
+      <Button>Button</Button>
+
+      <Card className={"w-[680px]"}>
+        <CardHeader>
+          <CardTitle>Notifications</CardTitle>
+          <CardDescription>You have 3 unread messages.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className=" flex items-center space-x-4 rounded-md border p-4">
+            <BellIcon />
+            <div className="flex-1 space-y-1">
+              <p className="text-sm font-medium leading-none">
+                Push Notifications
+              </p>
+              <p className="text-sm text-muted-foreground">
+                Send notifications to device.
+              </p>
+            </div>
+            <Switch />
+          </div>
+          <div>
+            {notifications.map((notification, index) => (
+              <div
+                key={index}
+                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+              >
+                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {notification.title}
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {notification.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">
+            <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+          </Button>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
